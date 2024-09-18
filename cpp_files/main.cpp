7764a4c1665da8f0a00fd86e3d45af7e91cb8596
Matvey787 <matveyklg@gmail.com>
2024-09-19 01:18:06 +0300

    fix buffer

--- cpp_files/main.cpp
+++ cpp_files/main.cpp
@@ -3,21 +3,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <stdint.h>
 
-void sortStrings(char** stringArray, size_t size);
-int needToSwap(char* firstStr, char* secondStr);
-void printArray(char** stringArray, size_t size);
-int my_strcmp(char* firstStr, char* secondStr);
-void getStrings(char*** stringArray, size_t* size, FILE* rFile, char** correctOrder);
-void printOneDemArray(int64_t* array, int size);
-void writeRepairedText(char** stringArray, FILE* file, size_t size);
-
-const int startMemForPointers = 8;
-
-struct s_text {
-    char** stringArray;
-    size_t numStr;
+struct s_string {
+    char* stringArray;
+    size_t lenOfStr;
 };
+
+
+
+void writeRepairedText(char** stringArray, FILE* file, size_t size);
+int needToSwap(s_string* firstStr, s_string* secondStr);
+static void mySwap(void* a, void* b, size_t elemMemory);
+void sortStrings(s_string* stringArray, size_t size);
+void getStrings(s_string* stringArray, size_t* size, FILE* rFile, char** correctOrder);
+void printArrayOfStructs(s_string* array, size_t size);
+int my_strcmp2(s_string* firstStr, s_string* secondStr);
+int my_strcmp(char* firstStr, char* secondStr);
+size_t getNumberOfStrings(FILE* rFile);
+
 
 int main(){
 
@@ -31,90 +35,92 @@ int main(){
     FILE* wFile = fopen(repairedTextFileName, "w");
     assert(rFile != NULL && "can't open read_File");
     assert(wFile != NULL && "can't open write_File");
-    //s_text text = {(char**)calloc(1, startMemForPointers), 0};
 
-
-    char** stringArray = (char**)calloc(1, startMemForPointers);
     assert(wFile != NULL && "can't allocate memory");
 
-    size_t readedStrings = 0;
-    char* correctOrder[100] = {};
+    size_t readedStrings = getNumberOfStrings(rFile);
 
-    getStrings(&stringArray, &readedStrings, rFile, correctOrder);
+    s_string* stringArray2 = (s_string*)calloc(readedStrings, sizeof(s_string));
+    assert(stringArray2 != NULL && "fatal realloc");
 
-    printOneDemArray((int64_t*)correctOrder, 100);
+    printf("%ld\n", readedStrings);
+    char** correctOrder = (char**)calloc(readedStrings, sizeof(char*));
 
-    sortStrings(stringArray, readedStrings);
-    printArray(stringArray, readedStrings);
+    getStrings(stringArray2, &readedStrings, rFile, correctOrder);
+    
+    printArrayOfStructs(stringArray2, readedStrings);
 
-    printf("Correct order:\n");
-    printArray((char**)correctOrder, readedStrings);
-
+    sortStrings(stringArray2, readedStrings);
+    printArrayOfStructs(stringArray2, readedStrings);
     writeRepairedText((char**)correctOrder, wFile, readedStrings);
 
     return 0;
 }
 
-void getStrings(char*** stringArray, size_t* size, FILE* rFile, char** correctOrder){
-    assert(*stringArray != NULL);
+void getStrings(s_string* stringArray, size_t* size, FILE* rFile, char** correctOrder){
+    assert(stringArray != NULL);
     assert(size != NULL);
     assert(rFile != NULL);
 
     char* buffer = NULL;
     size_t len = 0;
     ssize_t readedChars = 0;
-    size_t memForPointers = startMemForPointers;
     size_t readedStrings = 0;
     
     while ((readedChars = getline(&buffer, &len, rFile)) != EOF ){
-
-        if (++readedStrings * sizeof(char*) >  memForPointers){
-            memForPointers = memForPointers*4;
-            *stringArray = (char**)realloc(*stringArray, memForPointers);
-            assert(*stringArray != NULL && "fatal realloc");
-        }
-        (*stringArray)[readedStrings-1] = (char*)realloc((*stringArray)[readedStrings-1], readedChars*sizeof(char) - 1);
+        ++readedStrings;
+        stringArray[readedStrings-1].stringArray = (char*)realloc(stringArray[readedStrings-1].stringArray, readedChars*sizeof(char) - 1);
         
-        correctOrder[readedStrings-1] = (*stringArray)[readedStrings-1];
+        correctOrder[readedStrings-1] = stringArray[readedStrings-1].stringArray;
 
         for (int i = 0; i < readedChars - 1; i++)
-            (*stringArray)[readedStrings-1][i] = buffer[i];
+            stringArray[readedStrings-1].stringArray[i] = buffer[i];
             
-        (*stringArray)[readedStrings-1][readedChars - 1] = '\0';
+        stringArray[readedStrings-1].stringArray[readedChars - 1] = '\0';
+        stringArray[readedStrings-1].lenOfStr = readedChars - 1;
     }
     *size = readedStrings;
     fclose(rFile);
 }
 
-void sortStrings(char** stringArray, size_t size){
-    char* swapStr  = NULL;
+size_t getNumberOfStrings(FILE* rFile){
+    size_t numberOfStrings = 0;
+    fseek(rFile, 0, SEEK_END);
+    size_t size = ftell(rFile);
+    char* text = (char*)calloc(ftell(rFile), sizeof(char));
+    fseek(rFile, 0, SEEK_SET);
+    fread(text, sizeof(char), size, rFile);
+    for (size_t i = 0; i<size; i++){
+        if (text[i] == '\n') ++numberOfStrings;
+    }
+    fseek(rFile, 0, SEEK_SET);
+    free(text);
+    return numberOfStrings;
+}
+
+void sortStrings(s_string* stringArray, size_t size){
     for (size_t i = 0; i < size; i++){
-        for (size_t j = 0; j < size - 1; j++)
-            if (needToSwap(stringArray[j], stringArray[j+1])){
-                swapStr = stringArray[j];
-                stringArray[j] = stringArray[j+1];
-                stringArray[j+1] = swapStr;
+        for (size_t j = 0; j < size - i - 1; j++){
+            //printf("now %s %s ", stringArray[j].stringArray, stringArray[j+1].stringArray);
+            if (needToSwap(stringArray+j, stringArray + j + 1)){
+                //printf("need to swap %s %s\n", stringArray[j].stringArray, stringArray[j+1].stringArray);
+                mySwap(&stringArray[j], &stringArray[j+1], sizeof(s_string*));
             }
+        }
     }
 }
 
-int needToSwap(char* firstStr, char* secondStr){
-    if (my_strcmp(firstStr, secondStr) < 0)
+int needToSwap(s_string* firstStr, s_string* secondStr){
+    if ( /* my_strcmp(firstStr->stringArray, secondStr->stringArray) */ my_strcmp2(firstStr, secondStr) < 0)
         return 1;
     return 0;
 }
 
-void printArray(char** stringArray, size_t size){
-    for (size_t i = 0; i < size; i++){
-        printf("%s", stringArray[i]);
-        printf("\n");
-    }
-}
-
-void printOneDemArray(int64_t* array, int size){
-    int i = 0;
-    while (array[i] && (i < size)) {printf("%ld ", array[i]); i++;};
-    printf("\n");
+void printArrayOfStructs(s_string* array, size_t size){
+    printf("----------\n");
+    for (size_t i = 0; i < size; i++)
+        printf("string: %s len: %ld\n", array[i].stringArray, array[i].lenOfStr);\
+    printf("----------\n");
 }
 
 int my_strcmp(char* firstStr, char* secondStr){
@@ -134,10 +140,96 @@ int my_strcmp(char* firstStr, char* secondStr){
     return 0;
 }
 
+int my_strcmp2(s_string* firstStr, s_string* secondStr){
+
+    for (size_t i = firstStr->lenOfStr, j = secondStr->lenOfStr; i > 0 && j > 0; i--, j--){
+        
+        // first str
+        if (!isalpha(firstStr->stringArray[i]) && firstStr->stringArray[i] != '\0') { --i; ++j; continue; };
+
+        // second str
+        if (!isalpha(secondStr->stringArray[j]) && secondStr->stringArray[j] != '\0') { ++i; --j; continue; };
+
+        // if everithing is fine
+        if (tolower(firstStr->stringArray[i]) != tolower(secondStr->stringArray[j])){
+            //printf("| %c %c %d\n", firstStr->stringArray[i], secondStr->stringArray[j], (firstStr->stringArray[i]) - tolower(secondStr->stringArray[j]));
+            return tolower(firstStr->stringArray[i]) - tolower(secondStr->stringArray[j]);
+        }
+
+    }
+    return 0;
+}
+
 void writeRepairedText(char** stringArray, FILE* file, size_t size){
     for (size_t i = 0; i<size; i++){
         fputs(stringArray[i], file);
         fputs("\n", file);
     }   
     fclose(file);
+}
+
+static void mySwap(void* a, void* b, size_t elemMemory){
+    assert("mySwap: first element is null pointer" && a != NULL);
+    assert("mySwap: second element is null pointer" && b != NULL);
+    size_t summ = 0;
+
+    uint64_t* a_long = (uint64_t*)a;
+    uint64_t* b_long = (uint64_t*)b;
+
+
+    while (summ + sizeof(uint64_t) <= elemMemory){
+
+        uint64_t buf = *a_long;
+        *a_long = *b_long;
+        *b_long = buf;
+
+        a_long += 1;
+        b_long += 1;
+        summ += sizeof(uint64_t);
+
+    }
+    uint32_t* a_int = (uint32_t*)a_long;
+    uint32_t* b_int = (uint32_t*)b_long;
+
+    if (summ + sizeof(uint32_t) <= elemMemory){
+
+        uint32_t buf = *a_int;
+        *a_int = *b_int;
+        *b_int = buf;
+
+        a_int += 1;
+        b_int += 1;
+        summ += sizeof(uint32_t);
+
+    }
+
+    uint16_t* a_short = (uint16_t*)a_int;
+    uint16_t* b_short = (uint16_t*)b_int;
+
+    if (summ + sizeof(uint16_t) <= elemMemory){
+
+        uint16_t buf = *a_short;
+        *a_short = *b_short;
+        *b_short = buf;
+
+        a_short += 1;
+        b_short += 1;
+        summ += sizeof(uint16_t);
+
+    }
+
+    uint8_t* a_char = (uint8_t*)a_short;
+    uint8_t* b_char = (uint8_t*)b_short;
+
+    if (summ + sizeof(uint8_t) <= elemMemory){
+
+        uint8_t buf = *a_char;
+        *a_char = *b_char;
+        *b_char = buf;
+
+        a_char += 1;
+        b_char += 1;
+        summ += sizeof(uint8_t);
+
+    }
 }
